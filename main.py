6f766cae4c9a0da0cec0d8b879fa3a79f23b6f89
TutusBOT <barte77pl@gmail.com>
2022-05-01 21:02:20 +0200

    play command with wavelink

--- main.py
+++ main.py
@@ -1,36 +1,82 @@
 from cgi import test
 import nextcord
-from nextcord import Interaction
+from nextcord import Interaction, SlashOption, ChannelType
 from nextcord.ext import commands
 from nextcord.ext.commands import has_permissions, MissingPermissions
+from nextcord.abc import GuildChannel
 from keys import TOKEN
+import random
 import os
+import wavelink
 
 intents = nextcord.Intents.default()
 intents.members = True
 
-client = commands.Bot(intents=intents)
+bot = commands.Bot(intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
     print('TutuśBOT wylądował')
     print('==================')
+    bot.loop.create_task(node_connect())
 
-# @client.command()
+testServerId = 390518200787533834
+
+@bot.event
+async def on_wavelink_node_ready(node: wavelink.Node):
+    print(f'Node {node.identifier} is ready')
+
+async def node_connect():
+    await bot.wait_until_ready()
+    await wavelink.NodePool.create_node(bot=bot, host='lavalinkinc.ml', port=443, password="incognito", https=True)
+
+@bot.slash_command(guild_ids=[testServerId], description='Play a song!')
+async def play(interaction: Interaction, channel: GuildChannel = SlashOption(channel_types=[ChannelType.voice], description='Voice channel to join'), search: str = SlashOption(description='Song to play')):
+    await interaction.response.send_message('sth')
+    search = await wavelink.YouTubeTrack.search(query=search, return_first=True)
+    if not interaction.guild.voice_client:
+        vc: wavelink.Player = await channel.connect(cls=wavelink.Player)
+    elif not getattr(interaction.author.voice, 'channel', None):
+        return await interaction.send('First join a voice channel')
+    else:
+        vc: wavelink.Player = interaction.guild.voice_client
+    if vc.queue.is_empty and not vc.is_playing():
+        await vc.play(search)
+        await interaction.send(f'Now playing: {search.title}')
+    else:
+        vc.queue.put_wait(search)
+        await interaction.send(f'Added {search.title} to the queue.')
+    if vc.loop: return
+    setattr(vc, 'loop', False)
+
+
+@bot.slash_command(name='random', description='Generates a random number within a range of 1 and given number', guild_ids=[testServerId])
+async def randomnumber(interaction: Interaction, range:int):
+    await interaction.response.send_message(f'Random number between 1 and {range} is: {random.randrange(1,range)}')
+    
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(390518200787533836)
+    await channel.send('JD')
+
+
+
+bot.run(TOKEN)
+# @bot.command()
 # async def lud(ctx):
 #     await ctx.send('Rub mi louda nie żartuj')
 
-# @client.event()
+# @bot.event()
 # async def on_member_join(member):
-#     channel = client.get_channel(390518200787533836)
+#     channel = bot.get_channel(390518200787533836)
 #     await channel.send('JD')
 
-# @client.event()
+# @bot.event()
 # async def on_member_remove(member):
-#     channel = client.get_channel(390518200787533836)
+#     channel = bot.get_channel(390518200787533836)
 #     await channel.send(';0')
 
-# @client.command(pass_context = True)
+# @bot.command(pass_context = True)
 # async def lud(ctx):
 #     if(ctx.author.voice):
 #         channel = ctx.message.author.voice.channel
@@ -42,7 +88,7 @@ async def on_ready():
 #     else:
 #         await ctx.send('')
 
-# @client.command()
+# @bot.command()
 # async def join(ctx):
 #     if(ctx.author.voice):
 #         channel = ctx.message.author.voice.channel
@@ -50,19 +96,19 @@ async def on_ready():
 #     else:
 #         await ctx.send('You need to be in a voice channel to run this command.')
 
-# @client.command()
+# @bot.command()
 # async def leave(ctx):
 #     if(ctx.voice_client):
 #         await ctx.guild.voice_client.disconnect()
 #     else:
 #         await ctx.send('I am not in a voice channel.')
 
-# @client.command()
+# @bot.command()
 # async def random(ctx):
 #     await ctx.send(ctx.author)
 #     await voice.edit(ctx.author, reason="None", mute=True)
 
-# @client.command()
+# @bot.command()
 # @has_permissions(kick_members=True)
 # async def kick(ctx, member: nextcord.Member, *, reason=None):
 #     await member.kick(reason=reason)
@@ -73,7 +119,7 @@ async def on_ready():
 #     if isinstance(error, commands.MissingPermissions):
 #         await ctx.send("You don't have permission to kick people!")
 
-# @client.command()
+# @bot.command()
 # @has_permissions(ban_members=True)
 # async def ban(ctx, member: nextcord.Member, *, reason=None):
 #     await member.ban(reason=reason)
@@ -84,15 +130,3 @@ async def on_ready():
 #     if isinstance(error, commands.MissingPermissions):
 #         await ctx.send("You don't have permission to ban people!")
 
-testServerId = 390518200787533834
-
-@client.slash_command(name='Test', description='Test command', guild_ids=[testServerId])
-async def komenda(interaction: Interaction):
-    await interaction.response.send_message('test')
-
-@client.slash_command(guild_ids=[testServerId])
-async def random(interaction: Interaction):
-    await interaction.response.send_message(interaction)
-
-
-client.run(TOKEN)
